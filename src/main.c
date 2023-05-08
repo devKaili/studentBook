350bc08a16b7a33ec57b33c906b6fd8590cef340
Rostislav Otevřel <rostislav@otevrel.net>
2023-05-08 07:31:19 +0200

    refactor(main.c): add new functions to improve code readability and modularity feat(main.c): add input verification function to check if user input is within the available options feat(main.c): add menu select confirmation function to confirm user's input and ask if he/she wants to continue or change the option feat(main.c): add program path select function to select the appropriate function based on user's input docs(main.c): add comments to new functions and existing code

--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@
 
 char name[100];
 
-void intro(), personalization(), menu(), greet_the_user();
+void intro(), personalization(), menu(), greet_the_user(), menu_select_confirmation(int menu_select_one), program_path_select(int menu_select_one);
+int input_verification(int max_value, int input);
 
+// ------------------------------ MAIN FUNCTOIN ---------------------------------
 int main() {
     intro();
     personalization();
 }
+
 
 void intro() { // introduction to the program, wait for Enter at the end
     system("CLS");
@@ -21,9 +24,8 @@ void intro() { // introduction to the program, wait for Enter at the end
     printf("Stiskni Enter pro pokracovani...");
     getchar();
 }
-
 void personalization() { // personalize the app by specifying your name
-    printf("Zadej sve jmeno: ");
+    printf("Zadejte sve jmeno: ");
     scanf("%s", name);
     printf("Zadane jmeno: %s\n", name);
     sleep(0.5);
@@ -31,20 +33,74 @@ void personalization() { // personalize the app by specifying your name
     menu();
 }
 
+
 void menu() { // main menu
-    int menu_select;
+    int menu_select_one;
     system("CLS");
     printf("\033[1;36m--- ZAKOVSKA KNIHA ---\033[0;37m\n\n"); //cyan colored heading
     greet_the_user();
     printf("\n\n\033[1;36m--- HLAVNI MENU ---\033[0;37m\n");
-    printf("\n1) Zadat novou znamku\n2) Zobrazit prehled znamek\n3) Zobrazit prumery predmetu\n4) Zobrazit predmety, ze kterych zak propada\n4) Ukoncit program\n");
-    printf("\nZadej odpovidajici cislo a stiskni Enter: ");
-    scanf("%d", &menu_select);
+    printf("\n1) Zadat novou znamku\n2) Zobrazit prehled prumeru\n3) Zobrazit prumer vsech predmetu\n4) Zobrazit predmety, ze kterych zak propada\n5) Ukoncit program\n");
+    printf("\nZadejte odpovidajici cislo a stisknete Enter: ");
+    scanf("%d", &menu_select_one);
 
-    
+    // check if the selected option is available
+    if (input_verification(5, menu_select_one)) { // if the user selected an available option, the program will proceed
+        menu_select_confirmation(menu_select_one);
+    } else { // if the user selected an unavailable option, the program will spit out an error and ask for menu select again
+        printf("\n\n\033[1;31mZadana hodnota neodpovida moznostem v nabidce, zkuste to znovu...\033[0;37m\n\n");        
+        sleep(2);
+        menu();
+    }
 }
-
-void greet_the_user() {
+void menu_select_confirmation(int menu_select_one) { // print out user's input and ask if he/she wants to continue or change the option
+    char menu_option[50];
+    int menu_select_two;
+    system("CLS");
+    switch (menu_select_one) {
+        case 1:
+            strcpy(menu_option, "\n1 - Zadat novou znamku.");
+            break;
+        case 2:
+            strcpy(menu_option, "\n2 - Zobrazit prehled znamek.");
+            break;
+        case 3:
+            strcpy(menu_option, "\n3 - Zobrazit prumery predmetu.");
+            break;
+        case 4:
+            strcpy(menu_option, "\n4 - Zobrazit predmety, ze kterych zak propada.");
+            break;
+        case 5:
+            strcpy(menu_option, "\n5 - Ukoncit program.");
+            break;
+    }
+    printf("\n\033[0;33mVase volba:\033[0;37m%s\n", menu_option);
+    printf("\nPotvrdit nebo upravit volbu:\n1) Potvrdit volbu\n2) Upravit volbu");
+    printf("\nZadejte odpovidajici cislo a stisknete Enter: ");
+    scanf("%d", &menu_select_two);
+    if (input_verification(2, menu_select_two)) { // if the user selected an available option, the program will proceed
+        switch (menu_select_two) {
+            case 1:
+                program_path_select(menu_select_one);
+                break;
+            case 2:
+                menu();
+                break;
+        }
+    } else { // if the user selected an unavailable option, the program will spit out an error and ask for menu select again
+        printf("\n\n\033[1;31mZadana hodnota neodpovida moznostem v nabidce, zkuste to znovu...\033[0;37m\n\n");        
+        sleep(2);
+        menu_select_confirmation(menu_select_one);
+    }
+}
+int input_verification(int max_value, int input) { // check if user input exceeds min and max values
+    if (input >= 1 && input <= max_value) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+void greet_the_user() { // greet the user according to current time and his/her name
     char greet[50];
 
     time_t t = time(NULL);
@@ -63,4 +119,29 @@ void greet_the_user() {
     }
 
     printf("%s, %s.", greet, name);    
+}
+
+void program_path_select(int menu_select_one) {
+    switch (menu_select_one) {
+        case 1:
+            printf("OPTION ONE");
+            break;
+        case 2:
+            printf("OPTION TWO");
+            break;
+        case 3:
+            printf("OPTION THREE");
+            break;
+        case 4:
+            printf("OPTION FOUR");
+            break;
+        case 5:
+            printf("OPTION FIVE");
+            break;
+    }
+}
+
+
+void mark_input() {
+    
 }

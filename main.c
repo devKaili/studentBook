85360f87e61709485b6f3821d3fb1bc7a28da4bf
Rostislav Otevřel <rostislav@otevrel.net>
2023-05-22 15:40:30 +0200

    style(main.c): add comments to functions and improve readability feat(main.c): add function to greet user according to current time feat(main.c): add function to check user input and print error message if invalid feat(main.c): add function to confirm personalization settings feat(main.c): add function to exit program with confirmation menu refactor(main.c): change some variable names to improve readability

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
-// define color setting for console output
+// Define color setting for console output.
 #define BLACK "\033[0;30m"
 #define RED "\033[0;31m"
 #define GREEN "\033[0;32m"
@@ -19,24 +20,33 @@
 #define CYAN "\033[0;36m"
 #define WHITE "\033[0;37m"
 
-// clear the console output
+// Clear the console output.
 #define CLEAR printf("\e[1;1H\e[2J");
 
-void programIntroduction(), personalization(), mainMenu();
+// Function declaration.
+void programIntroduction(), personalization(), personalizationConfirm(int personalizationMenuSelect), invalidInput(), mainMenu(), programExit();
 
+char greetUserAccordingToTime();
 
+int inputCheck(int userInput, int maxValue);
+
+// Global variable declaration for the personalization() function.
 char name[100];
 int gender;
+
 
 // ------------------------------- MAIN FUNC STARTS HERE ---------------------------
 int main()
 {
     personalization();
 
+
+    return 0;
 }
 // ------------------------------- MAIN FUNC ENDS HERE ---------------------------
 
-// introduction to the app with explanation of what it does and also it's shortcomings
+
+// Introduction to the app with explanation of what it does and also it's shortcomings.
 void programIntroduction()
 {
     system("CLS");
@@ -46,7 +56,7 @@ void programIntroduction()
     getchar();
 }
 
-// this enables the user to personalize the app by specifying their name and gender, this info will be used for addressing the user correctly
+// This enables the user to personalize the app by specifying their name and gender, this info will be used for addressing the user correctly.
 void personalization()
 {
     CLEAR
@@ -55,32 +65,143 @@ void personalization()
     printf("Zadejte sve jmeno: ");
     scanf("%s", name);
     printf("Zadana hodnota: "YELLOW"%s\n"WHITE, name);
-    printf("Zvolte sve pohlavi(1 pro Muz, 2 pro Zena): ");
+    printf("Zvolte sve pohlavi(1 - Muz, 2 - Zena): ");
     scanf("%d", &gender);
-    printf("Zadana hodnota: "YELLOW"%d\n"WHITE, gender);
+    printf("Zadana hodnota: "YELLOW"%d\n"WHITE, gender);  
+
+    if (inputCheck(gender, 2))
+    {
+        invalidInput();
+        personalization();
+    }
+
+    personalizationConfirm(personalizationMenuSelect);
     
-    printf("\nJsi ");
-    (gender == 1) ? printf("spokojen ") : printf("spokojena ");
-    printf("se zadanymi hodnotami?\n1 - Ano, pokracovat do aplikace\n2 - Ne, upravit zadani\nZvol moznost a stiskni enter: ");
+}
+
+// Makes sure if the user is satisfied with the personalization settings.
+void personalizationConfirm(int personalizationMenuSelect)
+{
+    CLEAR
+    printf("Zvolene jmeno: "YELLOW"%s"WHITE"\nZvolene pohlavi: "YELLOW"%s"WHITE"\n\n", name, (gender == 1) ? "Muz" : "Zena");
+    printf("Jsi spokojen%c", (gender == 1) ? '\0' : 'a');
+    printf(" se zadanymi hodnotami?\n1 - Ano, pokracovat do aplikace\n2 - Ne, upravit zadani\nZvol moznost a stiskni enter: ");
     scanf("%d", &personalizationMenuSelect);
     printf("Zadana hodnota: "YELLOW"%d\n"WHITE, personalizationMenuSelect);
     sleep(1);
-    switch (personalizationMenuSelect)
-    {
-    case 1:
-        mainMenu();
-        break;
 
-    default:
-        personalization();
-        break;
+    if (inputCheck(personalizationMenuSelect, 2))
+    {
+        invalidInput();
+        personalizationConfirm(personalizationMenuSelect);
     }
+
+    (personalizationMenuSelect == 1) ? mainMenu() : personalization();
 }
 
-// main menu for the app, enables the user to select their desired action
-void mainMenu()
+// Check if the user input is in specified range, return 0 if input is ok and 1 if it's not.
+int inputCheck(int userInput, int maxValue)
 {
-    print(CYAN"----- HLAVNI MENU -----\n\n"WHITE);
-    
+    return (userInput >= 1 && userInput <= maxValue) ? 0 : 1;
+}
 
+// Prints an error message indicating that the input is invalid.
+void invalidInput()
+{
+    CLEAR
+    printf(RED"Zadana hodnota neodpovida moznostem v nabidce"WHITE);
+    sleep(2);
+    CLEAR
+}
+
+// Greet the user according to current time. AI generated.
+char greetUserAccordingToTime()
+{
+    char greet[20];
+    time_t t = time(NULL);
+    struct tm *currentTime = localtime(&t);
+    int hour = currentTime->tm_hour;
+
+    if (hour >= 5 && hour < 12) 
+    {
+        strcpy(greet, "Dobre rano");
+    } 
+    else if (hour >= 12 && hour < 18) 
+    {
+        strcpy(greet, "Dobre odpoledne");
+    } 
+    else 
+    {
+        strcpy(greet, "Dobry vecer");
+    }
+    printf("%s %s!\n", greet, name);
+}
+
+// Main menu for the app, enables the user to select their desired action.
+void mainMenu()
+{ 
+    int mainMenuSelect;
+    CLEAR
+    printf(CYAN"----- HLAVNI MENU -----\n\n"WHITE);
+    greetUserAccordingToTime();
+    printf("\nPrejes si...\n1 - Zadat novou znamku\n2 - Zobrazit tabulku prumeru\n3 - Zobrazit celkovy prumer a zda-li zak ukonci skolni rok s vyznamenanim\n4 - Zobrazit predmety ze kterych zak propada\n5 - Ukoncit program\n");
+    printf("Zvol operaci zadanim odpovidajiciho cisla: ");
+    scanf("%d", &mainMenuSelect);
+    printf("Zadana hodnota: "YELLOW"%d"WHITE, mainMenuSelect);
+    sleep(1);
+
+    if (inputCheck(mainMenuSelect, 5))
+    {
+        invalidInput();
+        mainMenu();
+    }
+
+    switch (mainMenuSelect)
+    {
+    case 1:
+        // markInput();
+        break;
+    case 2:
+        // TBD
+        break;
+    case 3:
+        // TBD
+        break;
+    case 4:
+        // TBD
+        break;
+    case 5:
+        programExit();
+        break;
+    }
+
+}
+
+void programExit()
+{
+    int progExitMenuSelect;
+    CLEAR
+    printf(RED"OPRAVDU SI PREJETE PROGRAM UKONCIT?"WHITE"\n\n1 - Ano, ukoncit (ztrata dat)\n2 - Ne, navrat do hlavniho menu\nZvol operaci zadanim odpovidajiciho cisla: ");
+    scanf("%d", &progExitMenuSelect);
+    printf("Zadana hodnota: "YELLOW"%d"WHITE, progExitMenuSelect);
+    sleep(1);
+
+    if (inputCheck(progExitMenuSelect, 2))
+    {
+        invalidInput();
+        programExit();
+    }
+
+    CLEAR
+    if (progExitMenuSelect == 1)
+    {
+        printf(RED"UKONCENI PROGRAMU\n"WHITE);
+        exit(0);
+    }
+    else
+    {
+        printf(GREEN"NAVRAT DO HLAVNIHO MENU\n"WHITE);
+        sleep(2);
+        mainMenu();
+    }
 }
